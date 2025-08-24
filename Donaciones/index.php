e4e0194f50e301b03b8dfe859e4c3222b7711761
Yisel-06 <yhernandez164@estudiantes.areandina.edu.co>
2025-08-24 01:41:13 -0500

    Conexión de la BD

---
+++ Donaciones/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Listado de Usuarios</title>
+</head>
+<body>
+    <table border="1">
+        <tr>
+            <th>Nombres</th>
+            <th>Apellidos</th>
+        </tr>
+        <?php
+            include "modelo/conexion.php";
+            $sql = $conexion->query("SELECT * FROM usuarios");
+            while ($datos = $sql->fetch_object()) {
+        ?>
+            <tr>
+                <td><?= $datos->nombres ?></td>
+                <td><?= $datos->apellidos ?></td>
+            </tr>
+        <?php } ?>
+    </table>
+</body>
+</html>
