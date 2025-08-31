d50ff82bde81e83f4caddbfbc81a7295d32d01df
julian11miranda <cmiranda22@estudiantes.areandina.edu.co>
2025-08-31 00:32:34 -0500

    Actualización conexión

--- Donaciones/Model/connection.php
+++ Donaciones/Model/connection.php
@@ -1,4 +1,20 @@
 <?php
-$conexion=new mysqli("localhost", "root", "", "bd_donaciones", "3306");
-$conexion->set_charset("utf8");
+class Connection {
+    private static $host = "localhost";
+    private static $user = "root"; // tu usuario de MySQL
+    private static $pass = "";     // tu contraseña
+    private static $db   = "bd_donaciones"; // tu base de datos
+    private static $conn;
+
+    public static function getConnection() {
+        if (!isset(self::$conn)) {
+            self::$conn = new mysqli(self::$host, self::$user, self::$pass, self::$db);
+
+            if (self::$conn->connect_error) {
+                die("Error en la conexión: " . self::$conn->connect_error);
+            }
+        }
+        return self::$conn;
+    }
+}
 ?>
